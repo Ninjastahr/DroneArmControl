836ae5f8c68cc9cabea03fac01881585c0d28305
Isaac <ihstahr@gmail.com>
2017-02-20 13:49:58 -0600

    Imported maestro library, added a constants file

---
+++ Constants.py
@@ -0,0 +1,14 @@
+#   There isn't a way to create true constants in Python, so please don't modify these in other classes
+#   Again: DO NOT MODIFY THESE VARIABLES
+
+#   Base constants:
+CONST_BASE_SWIVEL_CHANNEL = 1;
+CONST_BASE_ANGLE_START = 160;
+CONST_BASE_MIN_PWM = 2400;
+CONST_BASE_MAX_PWM = 9200;
+CONST_BASE_MAX_ANGLE = 180;
+
+#   Joint 1 constants:
+CONST_JOINT_1_CHANNEL = 2;
+CONST_JOINT_1_ANGLE_START = 30;
+
