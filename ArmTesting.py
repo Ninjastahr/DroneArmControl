c80b143d32757aebcc1df0ca912b2e954ad0f045
mppauk <mppauk@iastate.edu>
2017-02-20 16:15:39 -0600

    Arm Testing

--- ArmTesting.py
+++ ArmTesting.py
@@ -4,4 +4,7 @@ from Constants import *
 arm = ArmMovement.ArmControl()
 
 arm.setAngle(CONST_BASE_CHANNEL, 100)
-
+arm.setAngle(CONST_JOINT_1_CHANNEL,100)
+arm.setAngle(CONST_JOINT_2_CHANNEL,50)
+arm.setAngle(CONST_JOINT_3_CHANNEL,50)
+arm.setAngle(CONST_GRIPPER_CHANNEL,20)
