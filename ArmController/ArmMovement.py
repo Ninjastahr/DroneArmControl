24c47fa9ddb6359b1129e6d65ae7b9d76c2efe47
Isaac <ihstahr@gmail.com>
2017-02-27 15:29:54 -0600

    Fixed issues with channel constants

--- ArmController/ArmMovement.py
+++ ArmController/ArmMovement.py
@@ -3,50 +3,20 @@ from Constants import *
 
 
 class ArmControl:
-    armJointCurrAngle = [0, 0, 0, 0, 0]
-    armJointMaxAngle = [0, 0, 0, 0, 0]
-    armJointMinPWM = [0, 0, 0, 0, 0]
-    armJointMaxPWM = [0, 0, 0, 0, 0]
-    armJointSpeed = [0, 0, 0, 0, 0]
     armChannelDict = {
-        CONST_BASE_CHANNEL : 0,
-        CONST_JOINT_1_CHANNEL : 1,
-        CONST_JOINT_2_CHANNEL : 2,
-        CONST_JOINT_3_CHANNEL : 3,
-        CONST_GRIPPER_CHANNEL : 4
+        CONST_BASE_CHANNEL : [CONST_BASE_ANGLE_START, CONST_BASE_MIN_PWM, CONST_BASE_MAX_PWM,
+                              CONST_BASE_MAX_ANGLE, CONST_BASE_SPEED],
+        CONST_JOINT_1_CHANNEL : [CONST_JOINT_1_ANGLE_START, CONST_JOINT_1_MIN_PWM, CONST_JOINT_1_MAX_PWM,
+                                 CONST_JOINT_1_MAX_ANGLE, CONST_JOINT_1_SPEED],
+        CONST_JOINT_2_CHANNEL : [CONST_JOINT_2_ANGLE_START, CONST_JOINT_2_MIN_PWM, CONST_JOINT_2_MAX_PWM,
+                                 CONST_JOINT_2_MAX_ANGLE, CONST_JOINT_2_SPEED],
+        CONST_JOINT_3_CHANNEL : [CONST_JOINT_3_ANGLE_START, CONST_JOINT_3_MIN_PWM, CONST_JOINT_3_MAX_PWM,
+                                 CONST_JOINT_3_MAX_ANGLE, CONST_JOINT_3_SPEED],
+        CONST_GRIPPER_CHANNEL : [CONST_GRIPPER_ANGLE_START, CONST_GRIPPER_MIN_PWM, CONST_GRIPPER_MAX_PWM,
+                                 CONST_GRIPPER_MAX_ANGLE, CONST_GRIPPER_SPEED]
     }
 
     def __init__(self):
-        self.armJointCurrAngle[0] = CONST_BASE_ANGLE_START
-        self.armJointCurrAngle[1] = CONST_JOINT_1_ANGLE_START
-        self.armJointCurrAngle[2] = CONST_JOINT_2_ANGLE_START
-        self.armJointCurrAngle[3] = CONST_JOINT_3_ANGLE_START
-        self.armJointCurrAngle[4] = CONST_GRIPPER_ANGLE_START
-
-        self.armJointMinPWM[0] = CONST_BASE_MIN_PWM
-        self.armJointMinPWM[1] = CONST_JOINT_1_MIN_PWM
-        self.armJointMinPWM[2] = CONST_JOINT_2_MIN_PWM
-        self.armJointMinPWM[3] = CONST_JOINT_3_MIN_PWM
-        self.armJointMinPWM[4] = CONST_GRIPPER_MIN_PWM
-
-        self.armJointMaxPWM[0] = CONST_BASE_MAX_PWM
-        self.armJointMaxPWM[1] = CONST_JOINT_1_MAX_PWM
-        self.armJointMaxPWM[2] = CONST_JOINT_2_MAX_PWM
-        self.armJointMaxPWM[3] = CONST_JOINT_3_MAX_PWM
-        self.armJointMaxPWM[4] = CONST_GRIPPER_MAX_PWM
-
-        self.armJointMaxAngle[0] = CONST_BASE_MAX_ANGLE
-        self.armJointMaxAngle[1] = CONST_JOINT_1_MAX_ANGLE
-        self.armJointMaxAngle[2] = CONST_JOINT_2_MAX_ANGLE
-        self.armJointMaxAngle[3] = CONST_JOINT_3_MAX_ANGLE
-        self.armJointMaxAngle[4] = CONST_GRIPPER_MAX_ANGLE
-
-        self.armJointSpeed[0] = CONST_BASE_SPEED
-        self.armJointSpeed[1] = CONST_JOINT_1_SPEED
-        self.armJointSpeed[2] = CONST_JOINT_2_SPEED
-        self.armJointSpeed[3] = CONST_JOINT_3_SPEED
-        self.armJointSpeed[4] = CONST_GRIPPER_SPEED
-
         self.joint = maestro.Controller()
         self.joint.setRange(CONST_BASE_CHANNEL, CONST_BASE_MIN_PWM, CONST_BASE_MAX_PWM)
         self.joint.setRange(CONST_JOINT_1_CHANNEL, CONST_JOINT_1_MIN_PWM, CONST_JOINT_1_MAX_PWM)
@@ -56,16 +26,15 @@ class ArmControl:
 
 
     def getCurrentAngle(self, channel):
-        return self.armJointCurrAngle[self.armChannelDict[channel]]
+        return self.joint.getPosition(channel)
 
     def setAngle(self, channel, angle):
-        jointAnglePWM = angle * ((self.armJointMaxPWM[self.armChannelDict[channel]] -
-                                self.armJointMinPWM[self.armChannelDict[channel]]) /
-                                self.armJointMaxAngle[self.armChannelDict[channel]]) + \
-                                self.armJointMinPWM[self.armChannelDict[channel]]
-        self.joint.setTarget(self.armChannelDict[channel], jointAnglePWM)
-        self.joint.setAccel(self.armChannelDict[channel], self.armJointSpeed[self.armChannelDict[channel]])
-        self.armJointCurrAngle[self.armChannelDict[channel]] = angle
+        jointAnglePWM = angle * ((self.armChannelDict[channel][2] -
+                                self.armChannelDict[channel][1]) /
+                                self.armChannelDict[channel][4]) + \
+                                self.armChannelDict[channel][1]
+        self.joint.setTarget(channel, jointAnglePWM)
+        self.joint.setAccel(channel, self.armChannelDict[channel][5])
 
     def close(self):
         self.joint.close()
